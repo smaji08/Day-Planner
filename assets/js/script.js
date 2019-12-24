b0e42915add6a9cf7e5a3e75616770a42dbda804
Augustine <smaji08@gmailcom>
2019-12-24 11:30:07 -0500

    displaying according to the time done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+
     $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
+    $("span").hide();
+    
+    var x = moment().format("H");
+    x = parseInt(x);
+
+    for(var i=0;i<9;i++){
+        var y = $(".24hr").eq(i).text();
+        y = parseInt(y);
+
+        if (y === x){
+            $(".description").eq(i).addClass("present");
+        }
+        else if(y < x){
+            $(".description").eq(i).addClass("past");
+        }
+        else {
+            $(".description").eq(i).addClass("future");
+        }
+    }
     
 });
